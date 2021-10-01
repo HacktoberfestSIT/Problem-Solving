cff5207fdf783728811ca66287c8559c3e7c6293
Pankaj Bhadane <pankaj-kb>
2021-10-01 14:22:31 +0530

    Created Solution for Roman To decimal in Roman to Decimal/JavaScript/solution.js

---
+++ Roman To Decimal/solution.js
@@ -0,0 +1,64 @@
+<script>
+// Program to convert Roman
+// Numerals to Numberspublic
+    // This function returns
+    // value of a Roman symbol
+    function value(r) {
+        if (r == 'I')
+            return 1;
+        if (r == 'V')
+            return 5;
+        if (r == 'X')
+            return 10;
+        if (r == 'L')
+            return 50;
+        if (r == 'C')
+            return 100;
+        if (r == 'D')
+            return 500;
+        if (r == 'M')
+            return 1000;
+        return -1;
+    }
+ 
+    // Finds decimal value of a
+    // given romal numeral
+    function romanToDecimal( str) {
+        // Initialize result
+        var res = 0;
+ 
+        for (i = 0; i < str.length; i++) {
+            // Getting value of symbol s[i]
+            var s1 = value(str.charAt(i));
+ 
+            // Getting value of symbol s[i+1]
+            if (i + 1 < str.length) {
+                var s2 = value(str.charAt(i + 1));
+ 
+                // Comparing both values
+                if (s1 >= s2) {
+                    // Value of current symbol
+                    // is greater or equalto
+                    // the next symbol
+                    res = res + s1;
+                } else {
+                    // Value of current symbol is
+                    // less than the next symbol
+                    res = res + s2 - s1;
+                    i++;
+                }
+            } else {
+                res = res + s1;
+            }
+        }
+ 
+        return res;
+    }
+ 
+    // Driver Code
+     
+        // Considering inputs given are valid
+        var str = "MCMIV";
+        document.write("Integer form of Roman Numeral"
+        + " is " + romanToDecimal(str));
+</script>
