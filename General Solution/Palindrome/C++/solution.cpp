197d72e9c988d291838d2d74650077809d7e47a2
badasschef <hakkahman@gmail.com>
2021-10-16 04:24:25 +0530

    Added Solution for Palindrome in C++

---
+++ General Solution/Palindrome/C++/solution.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+        {
+            return false;
+        }
+        int rev = 0;
+        while (x > rev)
+        {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return x == rev || x == rev / 10;
+    }
+};
+
+//Note: int main is not given in this code.
